8f9833989b168a3db5b7f1c799450fea82f224d3
Mrinal Vig <mrinalvig@hotmail.com>
2017-09-27 15:38:48 -0700

    Assignment 10

---
+++ Assignment 10.h
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void factors (int);
+
+int main()
+{
+    cout << "Please enter a number, and I will display the prime factorization for it: " << endl;
+    int num;
+    cin >> num;
+
+    factors (num);
+   
+}
+
+void factors (int num)
+{
+     cout<<"The prime factors are: " << endl;
+
+    for (int i=2; i <= num; i++)
+    {
+     while(num % i == 0)
+     {
+       num /= i;
+       cout<< i << " " << endl;
+     }
+    }
+}
