70404bc4f31b62af16730dc5cb1e6cfd49d50772
Mrinal Vig <mrinalvig@hotmail.com>
2017-10-18 15:13:42 -0700

    Started Assignment 14

---
+++ Assignment 14.cpp
@@ -0,0 +1,7 @@
+#include <iostream>#include <string>#include <stack>using namespace std;
+void factorial(int);int fibonacci(int);
+int main(){    stack <string> call;    bool loop = true;        cout<<"Please select a positive integer" << endl;    int num;    cin >> num;        cout << "Would you like to implement a Factorial[1] or Fibonacci[2] method?" << endl;    int decision;    cin >> decision;        while(loop == true)    {        if (decision == 1)        {            call.push("Factorial" + num);            factorial(num);                        while(!call.empty())             {                cout << call.top() << " ";                call.pop();             } 
+            loop = false;        }        else if (decision == 2)        {            call.push("Fibonacci" + num);            cout << "The Fibonacci series at the " << num << "the number is: " << fibonacci(num) << endl;            loop = false;        }        else        {            cout << "This is not an option, please try again" << endl;            cin >> decision;        }    }    
+    return 0;}
+void factorial(int num){    int factorial = 1;        for(int i = 0; i <= num; ++i)    {        if (i == 0)        {            factorial = 1;        }        else        {            factorial = factorial * i;        }    }        cout << "The Factorial of " << num << " = " << factorial << endl;    }
+int fibonacci(int num){    int result = 1;        if (num <= 2)    {      return 1;    }    else    {        return fibonacci(num-1) + fibonacci(num-2);    }    }
